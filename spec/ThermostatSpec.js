f55a3d9bc134645d2778b6369871ce038e6e942f
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-07 16:28:21 +0100

    Power saving mode is on by default & has max & min temperatues depending on if it's on or off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -13,16 +13,50 @@ describe('Thermostat', function(){
   });
 
   it('allows temperature to be increased', function() {
-    thermostat.up(1)
+    thermostat.up();
     expect(thermostat.temperature).toEqual(21);
   });
 
   it('allow temperature to be decreased', function() {
-    thermostat.down(1)
+    thermostat.down();
     expect(thermostat.temperature).toEqual(19);
   });
 
   it('has a minimum temperature of 10', function() {
     expect(thermostat.minimum_temp).toEqual(10);
   });
+
+  it('has a power saving mode', function() {
+    expect(thermostat.toggle_power_saving).toBeDefined();
+  });
+
+  it('is set to power saving mode by default', function() {
+    expect(thermostat.power_saving_mode).toEqual(true);
+  });
+
+  it('can toggle between power saving mode, to be on or off', function() {
+    thermostat.toggle_power_saving()
+    expect(thermostat.power_saving_mode).toEqual(false);
+  });
+
+  it("won't increment temperature if it's already at maximum temp", function() {
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it("knows when it's at maximum temperature", function() {
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    thermostat.up();
+    expect(thermostat.isMaxTemp()).toEqual(true)
+  });
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,12 +3,35 @@
 var Thermostat;
 
 Thermostat = function() {
+  this.power_saving_mode = true;
   this.temperature = 20;
   Object.freeze(this.minimum_temp = 10);
+  Object.freeze(this.maximum_temp_with_power_saving = 25);
+  Object.freeze(this.maximum_temp_without_power_saving = 32);
 };
-Thermostat.prototype.up = function(increment){
-  this.temperature += increment;
+
+Thermostat.prototype.up = function(){
+  if (this.isMaxTemp()) {
+    return;
+  }
+  this.temperature += 1;
 };
-Thermostat.prototype.down = function(decrement){
-  this.temperature -= decrement;
+
+Thermostat.prototype.down = function(){
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.toggle_power_saving = function() {
+  if(this.power_saving_mode) {
+    this.power_saving_mode = false;
+  } else {
+    this.power_saving_mode = true;
+  }
+};
+
+Thermostat.prototype.isMaxTemp = function() {
+  if (this.power_saving_mode === false) {
+    return this.temperature === this.maximum_temp_without_power_saving;
+  }
+  return this.temperature === this.maximum_temp_with_power_saving;
 };
